e547f5bf66e0f33d919a7a3d109fdb5aea151669
tapo-arto <arto.huhta@tapojarvi.com>
2026-04-10 15:00:10 +0300

    Update my-aggregator-plugin.php

--- my-aggregator-plugin.php
+++ my-aggregator-plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Tapojärvi Jobs Infopaketit (V2)
  * Description: Tapojärvi Jobs Infopaketit – erillinen lisäosa RSS-synkronointiin, infopaketteihin, REST API:hin, Gutenberg-blokkiin ja Schema.org-merkintöihin.
- * Version: 4.1.3
+ * Version: 4.1.4
  * Author: Arto Huhta
  * Text Domain: tapojarvijobs
  * Requires at least: 5.8
